edd24149164ce301901c4f1e022369a58c92d560
Daisuke Hiraoka <dai@hiraoka-llc.co.jp>
2019-09-11 01:59:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ $host_name = gethostname();
 $ip = $_SERVER['REMOTE_ADDR'];
 echo "Hello World!<br>\n";
 echo "hostname = $host_name<br>\n";
-echo "ip address = $ip";
+echo "ip address = $ip<br>\n<br>\n";
 ?>
